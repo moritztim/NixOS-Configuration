f5a87f4ab76e5c13e27a963a3f88d1e07540b7c4
Moritz Tim W. <90388353+moritztim@users.noreply.github.com>
2024-11-23 12:32:41 +0100

    Minimize Privileges

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ NIXOS_KEXEC_INSTALLER_VERSION="v2.1.0"
 GET_CURRENT_ARCH_URL="https://raw.githubusercontent.com/moritztim/nixos-kexec-installer/refs/tags/$NIXOS_KEXEC_INSTALLER_VERSION/src/get_current_arch.sh"
 NIXOS_KEXEC_INSTALLER_URL="https://github.com/moritztim/nixos-kexec-installer/releases/download/$NIXOS_KEXEC_INSTALLER_VERSION/nixos-kexec.sh"
 
-echo "This script will remotely wipe the system \"$2\" and erect \"$1\" in its place."
+echo "This script will remotely wipe the system \"$3\" and erect \"$1\" in its place. It will login as \"$2\" on \"$3\"."
 echo "Are you sure you want to continue? [y/N]"
 read -r response
 
@@ -17,7 +17,7 @@ if [ "$response" != "y" ]; then
 fi
 
 echo "Remotely obtaining architecture..."
-architecture=$(ssh root@$2 "curl -sSLf $GET_CURRENT_ARCH_URL | bash")
+architecture=$(ssh $2@$3 "curl -sSLf $GET_CURRENT_ARCH_URL | bash")
 if [ -z "$architecture" ]; then
 	echo "Failed to obtain architecture."
 	exit 1
@@ -25,7 +25,7 @@ fi
 echo "Architecture: $architecture"
 
 echo "Remotely executing kexec installer image..."
-ssh root@$2 "bash <(curl -sSLf $NIXOS_KEXEC_INSTALLER_URL) --install"
+ssh $2@$3 "sudo bash <(curl -sSLf $NIXOS_KEXEC_INSTALLER_URL) --install"
 
 echo "Waiting for remote system to reboot..."
 while true; do ping -c1 nixos > /dev/null && break; done
